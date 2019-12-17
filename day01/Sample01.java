e8730b5a5a75f674eca3928ff321afbce77c8757
strading <stradingg@naver.com>
2019-12-17 13:28:55 +0900

    지각 ㅠ

--- day01/Sample01.java
+++ day01/Sample01.java
@@ -3,7 +3,7 @@ package day01;
 public class Sample01 {
 
 	public static void main(String[] args) {
-		System.out.println("hello 자바 1");
+		System.out.println("hello 자바 11");
 		System.out.println("hello 자바 2");
 		System.out.println("hello 자바 3");
 		
