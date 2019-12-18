b0aa9bba3eb946e4ac6139492fcf5ab55784dc8b
strading <stradingg@naver.com>
2019-12-18 15:07:31 +0900

    숫자

---
+++ python/01_numbers_variable.py
@@ -0,0 +1,19 @@
+# 저장
+
+# 1. 숫자
+a = 7
+b = 3
+
+print(a)
+
+print(a + b)
+print(a - b)
+print(a * b)
+
+print(a / b)
+print(a // b)   # 나눗셈의 몫만 출력
+
+print(a % b)
+# 나눴을때 몫 말고 나머지
+# 짝 / 홀 확인때 사용
+# 값 % 2
