c4ce28a6ee5d62fd57cba7c3ed897fb432793231
strading <stradingg@naver.com>
2019-12-18 15:09:12 +0900

    실습 완료

---
+++ python/04_example.py
@@ -0,0 +1,35 @@
+# print() happy hacking!
+hphk = "해피해킹"
+
+print(hphk)
+print(hphk)
+print(hphk)
+print(hphk)
+print(hphk)
+
+# 점심메뉴판
+
+menu1 = "순남 시레기"
+menu2 = "양자감"
+menu3 = "20층"
+
+print(menu1)
+print(menu2)
+print(menu3)
+
+# List
+menu = ["순남 시레기", "양자감", "20층..."]
+print(menu)
+print(menu[0])
+
+# 전화번호부 저장하기
+
+phone_num = ["02-0214-1231", "02-0214-1232", "02-0214-1233"]
+
+print(phone_num[0])
+
+# Dictionary
+
+dic_num = { "순남 시레기" : "02-0214-1231", "양자감" : "02-0214-1232", "20층" : "02-0214-1233" }
+
+print(dic_num["순남 시레기"])
